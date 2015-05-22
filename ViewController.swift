38fe767ea225b9a2de87b57cc2bbde0860f1d5a0
tro8.home <ro8@users.noreply.github.com>
2015-05-22 15:22:16 +0900

    first 1522
    first commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test-ios
+//
+//  Created by 青山 虎生 on 2015/05/22.
+//  Copyright (c) 2015年 tro8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
